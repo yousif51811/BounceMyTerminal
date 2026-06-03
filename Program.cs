c7bffe66faa25c8bcb0e9787d805b31d5ea5e39c
yousif51811 <yousif51811@gmail.com>
2026-06-03 14:40:12 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+namespace BounceMyTerminal
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr hwnd = WindowsAPI.FindWindow("CASCADIA_HOSTING_WINDOW_CLASS", null);
+            if (hwnd == IntPtr.Zero)
+                hwnd = WindowsAPI.GetConsoleWindow();
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+            // DVD Logo Ascii
+            Console.WriteLine("⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⡀\n⠀⢠⣿⣿⡿⠀⠀⠈⢹⣿⣿⡿⣿⣿⣇⠀⣠⣿⣿⠟⣽⣿⣿⠇⠀⠀⢹⣿⣿⣿\n⠀⢸⣿⣿⡇⠀⢀⣠⣾⣿⡿⠃⢹⣿⣿⣶⣿⡿⠋⢰⣿⣿⡿⠀⠀⣠⣼⣿⣿⠏\n⠀⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⠀⠀⢿⣿⣿⠏⠀⠀⢸⣿⣿⣿⣿⣿⡿⠟⠋⠁⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣸⣟⣁⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⣠⣴⣶⣾⣿⣿⣻⡟⣻⣿⢻⣿⡟⣛⢻⣿⡟⣛⣿⡿⣛⣛⢻⣿⣿⣶⣦⣄⡀⠀\n⠉⠛⠻⠿⠿⠿⠷⣼⣿⣿⣼⣿⣧⣭⣼⣿⣧⣭⣿⣿⣬⡭⠾⠿⠿⠿⠛⠉⠀");
+            Bounce(hwnd);
+        }
+
+        
+
+        private static void Bounce(IntPtr hwnd)
+        {
+            int windowWidth = 400;
+            int windowHeight = 400;
+
+            // Get initial screen size
+            var (screenWidth, screenHeight) = GetScreenSize();
+
+            int x = 0;
+            int y = 0;
+            // Speeds 
+            int dx = 5;
+            int dy = 5; 
+
+            // Ensure we don't start off-screen
+            if (screenWidth < windowWidth) windowWidth = screenWidth;
+            if (screenHeight < windowHeight) windowHeight = screenHeight;
+
+            while (true)
+            {
+                WindowsAPI.SetWindowPos(hwnd,
+                    new IntPtr(-1), // HWND_Topmost
+                    x, y, windowWidth, windowHeight,
+                    WindowsAPI.SWP_NOACTIVATE);
+                x += dx;
+                y += dy;
+                // Check Boundaries so we know to reverse the direction when an edge is hit.
+                // This is kinda funky on the terminal but whatever it works
+                // Right Edge
+                if (x + windowWidth >= screenWidth)
+                {
+                    x = screenWidth - windowWidth;
+                    dx = -dx;
+                }
+                // Left Edge
+                else if (x <= 0)
+                {
+                    x = 0;
+                    dx = -dx;
+                }
+
+                // Bottom Edge
+                if (y + windowHeight >= screenHeight)
+                {
+                    y = screenHeight - windowHeight;
+                    dy = -dy; 
+                }
+                // Top Edge
+                else if (y <= 0)
+                {
+                    y = 0;
+                    dy = -dy;
+                }
+
+                // Sleep to control speed and save CPU
+                // 16ms Should be around 60 FPS. (I think so)
+                Thread.Sleep(16);
+            }
+        }
+
+        private static (int screenWidth, int screenHeight) GetScreenSize()
+        {
+            WindowsAPI.SetProcessDPIAware();
+            int screenWidth = WindowsAPI.GetSystemMetrics(WindowsAPI.SM_CXSCREEN);
+            int screenHeight = WindowsAPI.GetSystemMetrics(WindowsAPI.SM_CYSCREEN);
+            return (screenWidth, screenHeight);
+        }
+    }
+}
---
+++ WindowsAPI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BounceMyTerminal
+{
+    public static class WindowsAPI
+    {
+        public const uint SWP_NOMOVE = 0x0002;
+        public const uint SWP_NOSIZE = 0x0001;
+        public const uint SWP_NOZORDER = 0x0004;
+        public const uint SWP_NOACTIVATE = 0x0010;
+        public const uint SWP_NOREDRAW = 0x0008;
+        public const int SM_CXSCREEN = 0;
+        public const int SM_CYSCREEN = 1;
+        [DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+    }
+}
